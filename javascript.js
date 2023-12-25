03466ad650965c08d3ff41e0716b702266a6fd56
SANTHOSH03S <150016638+SANTHOSH03S@users.noreply.github.com>
2023-12-25 10:07:43 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,61 @@
+var mainListDiv = document.getElementById("mainListDiv"),
+    mediaButton = document.getElementById("mediaButton");
+mediaButton.onclick = function () {
+    "use strict";
+    mainListDiv.classList.toggle("show_list");
+};
+function function1(){
+    window.location.href="https://youtube.com/"
+}
+function function2(){
+    window.location.href="https://web.whatsapp.com/"
+}
+function function3(){
+    window.location.href="https://www.facebook.com/"
+}
+function function4(){
+    window.location.href="https://www.instagram.com/"
+}
+function function5(){
+    window.location.href="https://chat.openai.com/"
+}
+function function6(){
+    window.location. href="https://mail.google.com/"
+}
+function function7(){
+    window.location. href="https://web.telegram.org/"
+}
+function function8(){
+    window.location. href="https://www.google.com/maps/"
+}
+function function9(){
+    window.location. href="https://www.amazon.in/"
+}
+function function10(){
+    window.location. href="https://www.flipkart.com/"
+}
+function function11(){
+    window.location. href="https://in.linkedin.com/"
+}
+function function12(){
+    window.location. href="https://twitter.com/"
+}
+function function13(){
+    window.location. href="https://open.spotify.com/"
+}
+function function14(){
+    window.location. href="https://online.kfc.co.in/"
+}
+function function_signup(){
+    window.location.href="signup.html"
+}
+function function_login(){
+    window.location.href="form.html"
+}
+function footer(){
+    window.location.href="#footer"
+}
+function about(){
+    window.location.href="#about"
+}
+
